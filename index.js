28f1041c5cb800054329761788950cf4a0e3e832
Nick Allen <nwestallen@gmail.com>
2020-12-11 12:31:41 -0500

    JS task 6 completed

--- index.js
+++ index.js
@@ -280,8 +280,17 @@ Use addArtist to do the following:
 
 Example: addArtist(artists) should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/){
-    /*Your Code Here*/
+function addArtist(array){
+    array.push({
+      id: 20,
+      name: "Nick Allen",
+      years: "1992 - 2020",
+      genre: "Web Design",
+      nationality: "United States",
+      bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    }
+    );
+    return array;
   }
 
   
